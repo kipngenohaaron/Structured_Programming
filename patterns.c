96edf280151d86615d444f61fad7272f2d31381b
kipngenohaaron <kipngenohaaron@gmail.com>
2025-06-18 18:18:11 +0300

    patterns modified

--- patterns.c
+++ patterns.c
@@ -256,3 +256,38 @@ int main() {
    * *
     *
 #include <stdio.h>  
+int main() {
+    int n = 5;
+
+    // Upper part of hollow diamond
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j < n; j++) {
+            printf(" ");
+        }
+        printf("*");
+        if (i > 1) {
+            for (int j = 1; j < (2 * i - 2); j++) {
+                printf(" ");
+            }
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // Lower part of hollow diamond
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = n; j > i; j--) {
+            printf(" ");
+        }
+        printf("*");
+        if (i > 1) {
+            for (int j = 1; j < (2 * i - 2); j++) {
+                printf(" ");
+            }
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
